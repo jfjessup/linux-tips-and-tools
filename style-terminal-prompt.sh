4735e1c07cda71ffd42cef26db9eba3273cd6b2d
James F Jessup <jfjessup@gmail.com>
2016-08-18 17:52:21 -0400

    adding milliseconds to the terminal prompt.

--- style-terminal-prompt.sh
+++ style-terminal-prompt.sh
@@ -1,11 +1,10 @@
-# Truns "username@username-computer:~/" into "[HH:MM:SS] ~/$ "
+# Truns "username@username-computer:~/" into "[HH:MM:SS.NNN] ~/$ "
 # AND
-# Turns "username@username-computer~/some/directory/" into "[HH:MM:SS] ~/some/directory/$"
+# Turns "username@username-computer~/some/directory/" into "[HH:MM:SS.NNN] ~/some/directory/$"
 
 if [[ $1 == '-h' ]] || [[ $1 == '-help' ]] ; then
-  echo "Set command prompt to '[HH:MM:SS] <directory from home>$'"
+  echo "Set command prompt to '[HH:MM:SS.NNN] <directory from home>$'"
   exit 0
 fi
 
-PS1="\[\033[36m\][\$(date +%H:%M:%S)] \w\$ \e[0m"
-
+PS1="\[\033[36m\][\$(date +%H:%M:%S.%3N)] \w\$ \e[0m"
